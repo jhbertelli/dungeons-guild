22e99c983f0942635ec3f080036bd7417ca44605
Bertelli <joaohenriquebertelli10@gmail.com>
2022-08-28 20:48:47 -0300

    historia e info geral personagem (1)

--- assets/resources/js/ficha.js
+++ assets/resources/js/ficha.js
@@ -1,7 +1,14 @@
 const carouselButtons = document.querySelectorAll('.carrosel-dentro ul li')
 
-for (let i = 0; i < carouselButtons.length; i++) {
-    // carouselButtons[i].addEventListener('click', () => {
-    //     $(document.querySelector('section').children[i]).slideToggle(300)
-    // })
+for (let i = 0; i < 4; i++) {
+    carouselButtons[i].addEventListener('click', () => {
+        $(document.querySelectorAll('section')[0].children[i]).slideToggle(300)
+    })
+    carouselButtons[i + 4].addEventListener('click', () => {
+        $(document.querySelectorAll('section')[1].children[i]).slideToggle(300)
+    })
 }
+
+carouselButtons[8].addEventListener('click', () => {
+    $(document.querySelector('.historia')).slideToggle(300)
+})
