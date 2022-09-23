1bd0d4210309d534d95fd685c281e9dae6b15346
Bertelli <joaohenriquebertelli10@gmail.com>
2022-09-22 22:22:28 -0300

    commit do rafael (página de cadastro)

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, send_from_directory
 
-app = Flask(__name__, static_folder='app/resources', template_folder='app/screens')
+app = Flask(__name__, static_folder='app/resources',
+            template_folder='app/screens')
 
 # static_folder é a pasta que contém arquivos de css, javascript, imagens
 # template_folder é a pasta que contém os htmls
@@ -16,39 +17,45 @@ def send_file(filename):  # torna disponível os arquivos da pasta 'assets'
     return send_from_directory(app.static_folder, filename)
 
 
-@app.route("/mundos")
-def mundos():
-    return render_template('mundos.html')
+@app.route("/cadastro")
+def cadastro():
+    return render_template('cadastro.html')
 
 
 @app.route("/personagens")
 def personagens():
     return render_template('personagens.html')
+
+
+@app.route("/mundos")
+def mundos():
+    return render_template('mundos.html')
 
 
 @app.route("/ficha")
 def ficha():
     return render_template('ficha.html')
 
+
 @app.route("/personagensvazio")
 def personagensvazio():
     return render_template('personagensvazio.html')
 
-@app.route("/personagens")
-def personagens():
-    return render_template('personagens.html')
 
 @app.route("/mundosvazio")
 def mundosvazio():
     return render_template('mundosvazio.html')
 
+
 @app.route("/embreve")
 def embreve():
     return render_template('embreve.html')
 
+
 @app.route("/livros")
 def livros():
     return render_template('livros.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
